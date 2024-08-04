69480fb53ba624a739603e81a76659acf5b5dd20
LuBaobei <hobairiku@gmail.com>
2024-08-04 17:33:30 +0800

    feat: example client

--- client.ts
+++ client.ts
@@ -3,8 +3,10 @@ const remoteTunnelPort = Bun.env.CALL_ME_BACK_TUNNEL_POST || 6654
 
 const args = process.argv.slice(2)
 
+// define the main command
 const mainCmd = 'code'
 
+// define the args that will be intercepted to what this command's args
 const interceptArgs = [
   "--remote",
   "ssh-remote+hy-coding",
@@ -27,7 +29,16 @@ const request = new Request(`http://localhost:${remoteTunnelPort}`, {
 
 try {
   const response = await fetch(request);
-  console.log(response)
+  const res = await response.json();
+  if(res.success) {
+    const out = `${res.stdout || res.stderr}`
+    if(out){
+      console.log()
+    }
+  }else{
+    console.error(res.stderr)
+    process.exit(1)
+  }
 } catch (err) {
   console.error(err)
 }
